486ebfc81a03990cd375040a35388cdf2b7e4ea6
Victor Lincoln <72924572+victorlincoln@users.noreply.github.com>
2021-04-13 00:49:08 -0300

    Add files via upload

---
+++ (If,ELSE)- operadora de celulares.py
@@ -0,0 +1,13 @@
+# Cálculo de bônus operadora de celulares
+
+pontuacao = int(input(" Informe sua pontuação: "))
+if pontuacao > 1000:
+    print("Você ganhou 3gb de bônus")
+else :
+    if pontuacao > 500:
+        print("Você ganhou 1,5gb de bônus")
+    else:
+        if pontuacao > 200:
+           print("Você ganhou 500mb de bônus")
+        else:
+           print("Você não ganhou nada")
