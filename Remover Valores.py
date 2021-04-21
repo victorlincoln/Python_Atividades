56b24addaf27f238ad5cffe0e0249b9f22b40869
Victor Lincoln <72924572+victorlincoln@users.noreply.github.com>
2021-04-21 02:00:32 -0300

    Add files via upload

---
+++ Remover Valores.py
@@ -0,0 +1,14 @@
+# Remover valores dentro de uma lista
+
+lista = ["Paulo" , "Mariana", "Ana", "Victor"]
+print(lista)
+
+#Remover o último valor digitado
+
+lista.pop()
+print(lista)
+
+# Remover um valor específico na sua lista
+
+lista.remove("Paulo")
+print(lista)
