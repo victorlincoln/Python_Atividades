4b9fa43a28b0a8a1321e929777c9b8aed936a722
Victor Lincoln <72924572+victorlincoln@users.noreply.github.com>
2021-04-21 03:35:30 -0300

    Add files via upload

---
+++ Parâmetros 2.py
@@ -0,0 +1,12 @@
+# Parâmetros nas funções
+def somar(a , b):
+
+    soma = a + b
+
+    print (soma)
+
+print("Olá alunos !")
+
+# É preciso chamar está função :
+
+somar(50, 100)
---
+++ Parâmetros.py
@@ -0,0 +1,14 @@
+# Parâmetros nas funções
+def somar(a , b):
+
+    soma = a + b
+
+    print (soma)
+
+print("Olá alunos !")
+valor1 = float(input("Digite um número: "))
+valor2 = float(input("Digite outro número: "))
+
+# É preciso chamar está função :
+
+somar(valor1, valor2)
