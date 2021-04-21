64f3688d38286ac6304a78a65c15606a57c4af27
Victor Lincoln <72924572+victorlincoln@users.noreply.github.com>
2021-04-21 03:24:02 -0300

    Add files via upload

---
+++ Função.py
@@ -0,0 +1,15 @@
+#Criando uma função
+#Método def
+
+def somar():
+    a = float(input("Digite um número: "))
+    b = float(input("Digite outro número: "))
+
+    soma = a + b
+
+    print (soma)
+
+print("Olá alunos !")
+# É preciso chamar está função :
+
+somar()
