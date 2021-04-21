486038ced27b023a0f6aad44f80e8df2f7808e86
Victor Lincoln <72924572+victorlincoln@users.noreply.github.com>
2021-04-21 02:29:34 -0300

    Add files via upload

---
+++ len e count.py
@@ -0,0 +1,11 @@
+#Para saber quantos elementos a lista possuí
+#Método len
+
+lista = ["Alan", "Julio", "Gaby", "Paulo"]
+
+print(len(lista))
+
+#Para saber quantas vezes determinado elemento está presente na lista
+#Método count
+
+print(lista.count("Alan"))
