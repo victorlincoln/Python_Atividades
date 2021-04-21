26f1a376b10ca7a2358d23c705d8e23378a4633c
Victor Lincoln <72924572+victorlincoln@users.noreply.github.com>
2021-04-21 02:20:17 -0300

    Add files via upload

---
+++ Sort e Reverse.py
@@ -0,0 +1,12 @@
+# Para colocar os valores em ordem alfabética
+# Método sort
+
+lista = ["Paulo" , "Mariana", "Ana", "Victor"]
+lista.sort()
+print(lista)
+
+#Para colocar os elementos em ordem contrária
+# Método Reverse
+
+lista.reverse()
+print(lista)
